89cc21e6adac866286f2709951edfd08f2296f79
Dowen Robinson <drobinson@ad.accelclaims.com>
2020-07-18 15:21:44 -0500

    Added pm2 and removed the use of sockets

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const http = require('http')
 const path = require('path')
 const fileSystem = require('fs')
 const cors = require('cors')
-const socketStream = require('socket.io-stream')
+
+
 
 
 const app = express()
@@ -11,54 +12,23 @@ const api = express()
 const PORT =  process.env.PORT || 9000
 
 const server = http.createServer(app)
-const io = require('socket.io')(server)
 
-io.on('connection', (socket) => {
-    const stream = socketStream.createStream()
-    socket.on('track', (trackid) => {
-        const filePath = path.resolve(__dirname, './private', `./${trackid}.mp3`)
-
-        //get file info
-        const stat = fileSystem.statSync(filePath)
-        const readStream = fileSystem.createReadStream(filePath)
-
-        readStream.pipe(stream)
-        socketStream(socket).emit('track-stream', stream, {stat})
-    })
-
-    
-
-    
-})
 
 app.use(cors())
+app.use(express.json())
+
+api.get('/track/:trackid', (req, res) => {
+    const trackid = req.params.trackid
+    const filePath = path.resolve(__dirname, `./private`, `./${trackid}.mp3`)
+    
+    
+    res.send(filePath)
+})
 
 api.get('/', (req, res) => {
-    res.send('THIS IS A TEST SERVER.')
+    res.send('<h2>Server is running</h2>')
 })
  
-api.get('/song/:id', (req, res) => {
-    // generate file path
-    
-    const filePath = path.resolve(__dirname, './private', `./${req.params.id}.mp3`)
-
-    //get file size info
-    const stat = fileSystem.statSync(filePath)
-
-    //set response headers
-    res.writeHead(200, {
-        'Content-Type': 'audio/mpeg',
-        'Content-Length': stat.size
-    })
-    console.log(`File size is ${stat.size}`)
-    
-    //create read stream
-    const readStream = fileSystem.createReadStream(filePath)
-    
-    // attach this stream with response stream
-
-    readStream.pipe(res)
-})
 
 app.use('/', api)
 
